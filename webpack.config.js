8c0e0a0b14557b1d9f5ccaf974a2bae69c10afee
Max White <mushishi78@gmail.com>
2015-09-03 23:56:01 +0100

    Remove hjs-webpack boiler plate and relace with simple config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,22 @@
-var getConfig = require('hjs-webpack');
+var webpack = require("webpack");
 
-module.exports = getConfig({
-  in: 'src/app.js',
-  out: 'public',
-  clearBeforeBuild: '!(icons|favicon.ico)',
-  html: function (data) {
-    return {
-      'index.html': html(data),
-      '200.html': html(data),
-      '404.html': html(data)
-    }
-  }
-});
-
-function html(data) {
-  var sep = data.relative ? '' : '/';
-
-  return [
-    "<!DOCTYPE html>",
-    "<html>",
-    "<head>",
-    "<meta charset='utf-8'>",
-    "<meta http-equiv='X-UA-Compatible' content='IE=edge'>",
-    "<meta name='viewport' content='width=device-width, initial-scale=1'>",
-    "<meta name='description' content='Content Editor for Static-Sites hosted on GitHub'>",
-    "<title>Content Editor</title>",
-    "<link href='https://fonts.googleapis.com/css?family=Ubuntu|Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>",
-    "<link rel='stylesheet' href='" + sep + data.css + "'/>",
-    "</head>",
-    "<body>",
-    "<script src='" + sep + data.main + "'></script>",
-    "</body>",
-    "</html>"
-  ].join('');
-}
+module.exports = {
+  entry: __dirname + '/src/app.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style-loader!css-loader" }
+    ]
+  },
+  devServer: {
+    contentBase: './public',
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({ minimize: true })
+  ]
+};
